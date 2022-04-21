3b59fe69d759c539048e81d14b824fa4f5b646f3
Bruno Andrade <bruno77sa@gmail.com>
2022-04-21 14:33:59 -0300

    Add and config tests in github action.

--- config/test.exs
+++ config/test.exs
@@ -7,8 +7,9 @@ import Config
 # Run `mix help test` for more information.
 config :pipeline_ci, PipelineCi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   hostname: "localhost",
+  port: 5433,
   database: "pipeline_ci_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
